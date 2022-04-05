3a0c3ebf5ab157ee2ad6a0b4f4afd1759d07d938
Na_youngHoon <38518648+younghoonNa@users.noreply.github.com>
2022-04-05 10:08:27 +0900

    Delete 09_draw_histogram.py

--- Video processing/week6_histogram_and_hist_stretching/09_draw_histogram.py
+++
@@ -1,9 +0,0 @@
-import numpy as np, cv2
-
-def draw_histo(hist, shape=(200, 256)):
-    hist_img = np.full(shape, 255, np.uint8)
-    cv2.normalize(hist, hist, 0, shape[0], cv2.NORM_MINMAX)
-    gap = hist_img.shape[0]/hist_img.shape[1]
-
-
-    for
